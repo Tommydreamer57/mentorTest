35551175f4347956d67795305ecac18fff8abfaa
Thomas Lowry <minilao94@yahoo.com>
2017-12-12 13:25:35 -0700

    done

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,42 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt(str) {
+    return str.split('').reverse().join('')
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups(array) {
+    return array.reverse().filter((a, i, arr) => i === arr.lastIndexOf(a)).reverse()
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt(str) {
+    return str.split(' ').map((str, i) => str.split('').map((a, j) => j === 0 ? a.toUpperCase() : a.toLowerCase()).join('')).join(' ')
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    return str.split('').reduce((a, b) => a + (vowels.includes(b) * 1), 0)
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime(n) {
+    if (n === 1 || n === -1) return false
+    for (let i = 2; i <= (n / 2); i++) {
+      if (n % i === 0) return false
+    }
+    return true
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +45,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: "1020",
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +56,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1: "Hello World",
+  log2: undefined
 }
